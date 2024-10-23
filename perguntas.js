e704c0878ebcb912dff967e59e503a6a712353c1
nycolleleahz <nycolle.leirias.silva@escola.pr.gov.br>
2024-10-23 11:12:14 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,41 @@
+criaCartao(
+    'Conhecimentos Gerais',
+    'Quem foi a primeira pessoa a viajar no Espaço?',
+    'Yuri Gagarin.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Conhecimentos Gerais',
+    'Qual a montanha mais alta do mundo?',
+    'Monte Everest.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz tchau em Inglês?',
+    'Oi em ingles é BYE (BAI)'
+)
+
+criaCartao(
+    'Conhecimentos Gerais',
+    'Onde se localiza Machu Picchu?',
+    'Peru'
+)
+
+criaCartao(
+    'Conhecimentos Gerais',
+    'Que país tem o formato de uma bota?',
+    'Itália.'
+)
+
+criaCartao(
+    'Conhecimentos Gerais',
+    'Quem inventou a lâmpada?',
+    'Thomas Edison'
+)
